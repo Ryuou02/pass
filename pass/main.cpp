c0ec3c262921b940e19ef87e770cdd63526ba9ec
Nikhil Tom Jose <133224167+Ryuou02@users.noreply.github.com>
2025-07-02 11:46:27 +0530

    Update main.cpp

--- pass/main.cpp
+++ pass/main.cpp
@@ -1,12 +1,8 @@
-#include "pass.h"
+#include "commandLine.h"
 
 int calc(int k, const char** args, commandLine* obj);
 
 int main(int k, const char** args) {
-	pass passObj;
-	passObj.addCommand("calc", calc);
-	std::string calcCommands[] = { "sin","cos","tan" };
-	passObj.passCLI.addAutocompletePrevWordChange("calc", calcCommands, std::size(calcCommands));
-
-	passObj.pass_exec(k - 1, args + 1);
+	commandLine obj;
+	calc(k,args,&obj);
 }
